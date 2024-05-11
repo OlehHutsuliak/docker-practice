2b995ddad5da627bdc103846b3e97c9284ece5c6
olehhutsuliak <olegucul@gmail.com>
2024-05-11 17:57:21 +0200

    update travis config

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 for (let i = 0; i <= 5; i++) {
   test(`Test @${i}`, () => {
     const randomNumber = Math.floor(Math.random() * 10000) / 100;
-    console.log(randomNumber);
     expect(typeof randomNumber).toBe('number');
   });
 }
